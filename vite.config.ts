fa44ddc80c14f89d2395bf80ae09e72f90491bcf
Yuniesky Lemus <yuniesky.lemus90@gmail.com>
2026-09-19 15:41:00 -0400

    Fix production D1 binding

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { readExecutionProfile } from "./scripts/execution-profile.mjs";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "99ef2c21-17a8-4ce1-bc2d-b9037598a2e1";
 
 const { d1, r2 } = hostingConfig;
 
